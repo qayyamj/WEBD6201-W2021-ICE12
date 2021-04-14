c88dd9b55cce05416eebeef398eaf77d43e1a884
qayyamj <qayyamj@gmail.com>
2021-04-14 13:21:19 -0400

    switched to my database on mongodb

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,4 +1,4 @@
 export const LocalURI = "mongodb://localhost/webd6201";
-export const RemoteURI = "mongodb+srv://thomas:5wWyrxbVa59twJoh@webd6201.zvqij.mongodb.net/webd6201?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://qayyamj:uvdluAWD1hRlU4y4@webd6201.1qnhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 export const Secret = "someSecret";
 export const Host = "MongoDB Atlas";
